24a88b25c5dd3ebbdfc5764ad70b77b2d4a02b4d
saul <43302778+saulwolfcode@users.noreply.github.com>
2020-04-16 01:44:19 -0300

    creando las vistas de chat

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,8 +1,20 @@
-import React from 'react';
+// import React from 'react';
+
+import React, {useState, useEffect} from 'react';
+import querystring from "query-string";
+import io from "socket.io-client";
 import "./Chat.scss";
-const Chat = () => {
+const Chat = ({location}) => {
+      const [name, setName] = useState("");
+      const [room, setRoom] = useState("");
+    useEffect(()=>{
+        const {name, room} = querystring.parse(location.search)
+            
+        setName(name);
+        setRoom(room);
+    })
     return (<div>
-        chat
+       <h1>CHAT</h1>
     </div>);
 }
  
--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -2,37 +2,32 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Join.scss";
 const Join = () => {
-  const { name, setName } = useState("");
-  const { room, setRoom } = useState("");
+  const [ name, setName] = useState("");
+  const [room, setRoom] = useState("");
   return (
     <div className="joinOuterContainer">
       <div className="joinInnerContainer">
-        <div className="heading">
-          <div>
-            <input
-              type="text"
-              className="joinInput"
-              placeholder="name"
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              className="joinInput mt-20"
-              placeholder="room"
-              onChange={(e) => setRoom(e.target.value)}
-            />
-          </div>
-          <Link
-            onClick={(e) => (!name || !room ? e.prevetDefault() : null)}
-            to={`/chat?name${name}&${room}`}
-          >
-            <button type="submit mt-20" className="button">
-              Sign In
-            </button>
-          </Link>
+        <div className="heading">JOIN</div>
+        <div>
+          <input
+            type="text"
+            className="joinInput"
+            placeholder="name"
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
+        <div>
+          <input
+            type="text"
+            className="joinInput mt-20"
+            placeholder="room"
+            onChange={(e) => setRoom(e.target.value)}
+          />
+        </div>
+        <Link onClick={e => (!name || !room) ? e.preventDefault() : null}
+          to={`/chat?name=${name}&room=${room}`}>
+          <button className={"button mt-20"} type="submit">Sign In</button>
+        </Link>
       </div>
     </div>
   );
