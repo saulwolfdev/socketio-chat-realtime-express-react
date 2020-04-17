53dd6118f00793fa34c7ae21b621679c24509d5d
saul <43302778+saulwolfcode@users.noreply.github.com>
2020-04-17 01:45:09 -0300

    comunicandose entre el front con el back y generando los mensajes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express=require("express");
 const sockeio = require("socket.io");
-const http=require("http")
-const PORT=process.env.PORT||5000
+const http=require("http");
+
+const   {addUser,removeUser,getUser,getUsersInRoom} = require("./users");
+
+
+const PORT=process.env.PORT||5000;
 
 const router=require("./router")
 const app=express()
@@ -9,7 +13,22 @@ const server=http.createServer(app);
 const io=sockeio(server);
 
 io.on("connection",(socket)=>{
-    console.log("conection")
+    console.log("we have a new conection");
+    socket.on("join",({name,room},callback)=>{
+        console.log(name,room)
+    const { error, user } = addUser({ id: socket.id, name, room });
+    if (error) return callback(error);
+    
+    socket.emit("message",{user:"admin",text:`${user.name}, Welcome to the room ${user.room}`});
+    socket.broadcast.to(user.room).emit("message",{user:"admin",text:`${user.name}, has joined!:)`});
+    
+    socket.join(user.room);
+    callback()
+    })
+    socket.on("sendMessage",(message,callback)=>{
+        const user=getUser(socket.id);
+      io.to(user.room).emit("message",{user:user.name,text:message}); 
+    })
     socket.on("disconnect",()=>{
         console.log("user had left")
     })
