371deec0d8801c318861bef53fe1297be8f907aa
saul <43302778+saulwolfcode@users.noreply.github.com>
2020-04-18 18:51:45 -0300

    comunicion completa entre chat en tiempo real, entre salas, el servidor responde desde express y en el front se enlaza con el backend

--- server/index.js
+++ server/index.js
@@ -1,31 +1,36 @@
 const express=require("express");
 const sockeio = require("socket.io");
 const http=require("http");
-
+const cors=require("cors");
+ 
 const   {addUser,removeUser,getUser,getUsersInRoom} = require("./users");
-
-
 const PORT=process.env.PORT||5000;
 
-const router=require("./router")
-const app=express()
+const router=require("./router");
+const app=express();
+
 const server=http.createServer(app);
 const io=sockeio(server);
 
 io.on("connection",(socket)=>{
-    console.log("we have a new conection");
-    
+    console.log("we have a new conection in server index.js");
     socket.on("join",({name,room},callback)=>{
-        console.log(name,room)
+    console.log(name,room)
     const { error, user } = addUser({ id: socket.id, name, room });
     if (error) return callback(error);
     
-    socket.emit("message",{user:"admin",text:`${user.name}, Welcome to the room ${user.room}`});
-    socket.broadcast.to(user.room).emit("message",{user:"admin",text:`${user.name}, has joined!:)`});
+    socket.emit("message",{
+        user:"admin", 
+        text:`${user.name}, Welcome to the room ${user.room}`,
+    });
+    socket.broadcast
+    .to(user.room)
+    .emit("message",{user:"admin",text:`${user.name}, has joined! :-)`});
     
     socket.join(user.room);
     callback()
     })
+    
     socket.on("sendMessage",(message,callback)=>{
         const user=getUser(socket.id);
       io.to(user.room).emit("message",{user:user.name,text:message});
@@ -38,7 +43,7 @@ io.on("connection",(socket)=>{
 
 
 
-app.use(router)
-
+app.use(router);
+app.use(cors());
 
 server.listen(PORT,()=>console.log(`server has started on port ${PORT}`))
--- server/users.js
+++ server/users.js
@@ -1,23 +1,22 @@
    
-   const users=[]
-   
+   const users=[];
    const addUser=({id,name,room})=>{
            name = name.trim().toLowerCase();
            name = room.trim().toLowerCase();
 
            const existingUser = users.find((user) =>user.room===room &&user.name===name);
-           if(existingUser){
-               return {error:"Username is taken"}
-           }
+            if (!name || !room)return { error: "Username and room are required." };
+            if (existingUser) return { error: "Username is taken." };
+            
             const user={id,name,room};
+            
             users.push(user)
             return {user}
    }
     const removeUser = (id) =>{
         const index=users.findIndex((user)=>user.id===id);
-        if(index!==1){
-            return users.splice(index,1)[0];
-        }
+        if(index!==1) return users.splice(index,1)[0];
+        
     };
     const getUser = (id) =>users.find((user)=>user.id===id);
     const getUsersInRoom = (users) =>users.filter((user)=>user.room===room);
